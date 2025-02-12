fb6936771f96da9d2fb146df085113cb64d40f19
363449299 <363449299@2260I-L29-032>
2025-02-12 14:36:56 -0500

    Add project files.

---
+++ Topic 1 - Printing and Commands (2)/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Topic_1___Printing_and_Commands__2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sophie
+            Console.WriteLine("Hello, Sophie");
+            Console.WriteLine("Computers are better than bio");
+            Console.WriteLine("Comments are used because they make the program easy to read and follow");
+            Console.WriteLine("");
+            
+        }
+    }
+}
